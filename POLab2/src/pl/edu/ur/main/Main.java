c5b7d522254c0f413c8a0df60f35c86d2d5b33b9
student <student@pc-108-12>
2018-03-07 18:06:24 +0100

    Elo

--- POLab2/src/pl/edu/ur/main/Main.java
+++ POLab2/src/pl/edu/ur/main/Main.java
@@ -1,5 +1,7 @@
 package pl.edu.ur.main;
 
+import java.util.Scanner;
+
 /**
  *
  * @author marcin
@@ -9,8 +11,98 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO zad 3, 4, 6, 7
-    }
     
-}
+    public static void main(String[] args) {
+        
+        System.out.println("Podaj wspolczynniki a b i c");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        int wyrazenie= a*x^2+b*x+c;
+        
+        if(delta>0){
+        
+            x1=(-b-sqrt(delta))/2*a;
+        }
+        
+        
+        
+    }
+    }
+        
+        
+        /*
+        outer:
+        for(int i=0;i<20;i++){
+            if (i==2 || i ==6 || i==9 || i==15 || i==19)
+            continue;
+           
+           System.out.println(i);
+        
+        }
+        */
+    
+    /*    
+        System.out.println("Podaj liczbe");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        
+        int i=0;
+        while(i<100){
+            System.out.println("Petla numer: "+i);
+            System.out.println("a="+a);
+            a+=1;
+            i++;
+        }
+        
+    }}
+
+      /* 
+        System.out.println("Podaj liczbe");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        {
+        switch (a) {
+            case 1:
+                System.out.println("Styczen");
+                break;
+            case 2:
+                System.out.println("Luty");
+                break;
+            case 3:
+                System.out.println("Marzec");
+                break;
+            case 4:
+                System.out.println("Kwiecien");
+                break;
+            case 5:
+                System.out.println("Maj");
+                break;
+            case 6:
+                System.out.println("Czerwiec");
+                break;
+            case 7:
+                System.out.println("Lipiec");
+                break;
+            case 8:
+                System.out.println("Sierpien");
+                break;
+            case 9:
+                System.out.println("Wrzesien");
+                break;
+            case 10:
+                System.out.println("Pazdziernik");
+                break;
+            case 11:
+                System.out.println("Listopad");
+                break;
+            case 12:
+                System.out.println("Grudzien");
+                break;
+        }
+        }
+    }*/
+
+      
+
